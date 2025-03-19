f58832023fd8f055c93ec8d6e2e8fee207c6bce1
THISAGAPADMANANDA <thisagabusiness@gmail.com>
2025-03-19 13:03:57 +0530

    init 2

--- resources/js/Components/Core/InputLabel.tsx
+++ resources/js/Components/Core/InputLabel.tsx
@@ -10,11 +10,11 @@ export default function InputLabel({
         <label
             {...props}
             className={
-                `block text-sm font-medium text-gray-700 dark:text-gray-300 ` +
+                `labe ` +
                 className
             }
         >
-            {value ? value : children}
+       <span className={"label-text"}>{value ? value : children}</span>
         </label>
     );
 }
